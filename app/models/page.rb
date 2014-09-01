0131cc6072b2aada3dfb7163ea9026ae5068fe06
astrsk-hori <horie_go_gn@cyberagent.co.jp>
2014-09-01 13:07:04 +0900

    add validate to model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  validates_presence_of :title
+  validates_presence_of :body
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -23,7 +23,7 @@ describe PagesController do
   # This should return the minimal set of attributes required to create a valid
   # Page. As you add validations to Page, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) { { "title" => "MyString", "body" => "MyString" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
